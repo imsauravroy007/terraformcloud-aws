baa1d91c44fb0c55d74dddcb82d6f2090911ed4c
sauravroy12 <saurav.roy@tabnerglobal.com>
2025-08-15 23:34:24 -0400

    new files

--- main.tf
+++ main.tf
@@ -1,27 +1,10 @@
 
 
 
-resource "fakewebservices_vpc" "primary_vpc" {
-  name       = "Primary VPC"
-  cidr_block = "0.0.0.0/1"
-}
-
-resource "fakewebservices_server" "servers" {
-  count = 2
-
-  name = "Server ${count.index + 1}"
-  type = "t2.micro"
-  vpc  = fakewebservices_vpc.primary_vpc.name
-}
-
-
-
-resource "fakewebservices_load_balancer" "primary_lb" {
-  name    = "Primary Load Balancer"
-  servers = fakewebservices_server.servers[*].name
-}
-
-resource "fakewebservices_database" "prod_db" {
-  name = "Production DB"
-  size = 256
+resource "aws_vpc" "myvpc" {
+  
+  cidr_block = "10.0.0.0/16"
+  tags = {  
+    Name = "myvpc"
+  }
 }
